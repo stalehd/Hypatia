e83129fa960a2759a84ffca27856f53e48452b64
Ståle Dahl <stalehd@comoyo.com>
2014-12-14 23:14:46 +0100

    Fix wording in license header

--- hypatia.interpolate.js
+++ hypatia.interpolate.js
@@ -1,5 +1,5 @@
 /*
-    Hypatia: Gradient, interpolation and heatmap demo
+    Hypatia: Interpolation (ugly version)
     Copyright (C) 2014 Ståle Dahl <stalehd@gmail.com>
 
     This program is free software; you can redistribute it and/or modify
